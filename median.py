7ad74cf5eb072802722bcdcdb7d257f0ad1ba277
Chisom Ikechukwu <ik3chukwuchisom28@gmail.com>
2024-10-05 21:13:57 +0100

    create median calculator

--- median.py
+++ median.py
@@ -1,6 +1,21 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+def median_calc(arr):
+   arr_sorted = sorted(arr)
+   length_arr = len(arr)
+   median_id = length_arr//2
+   print(median_id)
+   if length_arr % 2 == 0:
+       
+       median = (arr_sorted[median_id] + arr_sorted[median_id -1])/2
+       return median
+   else:
+       median = (arr_sorted[median_id])
+       return median 
 
+
+
+
+       
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -10,3 +25,7 @@ while True:
     else:
         break
 print(numbers)
+
+median_value = median_calc(numbers)
+
+print(f"The median is: {median_value}")
